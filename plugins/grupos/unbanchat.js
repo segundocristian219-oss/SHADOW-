4c3fc1f79ead51072f2c13e55ddfa1d41e61d1c8
segundocristian219-oss <segundocristian219@gmail.com>
2026-01-21 13:19:27 -0600

    Crear unbanchat.js

---
+++ plugins/grupos/unbanchat.js
@@ -0,0 +1,11 @@
+let handler = async (m, { conn, isAdmin, isROwner} ) => {
+    if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
+    global.db.data.chats[m.chat].isBanned = false
+    m.reply('🔓 Bot activo en este grupo.')   
+}
+handler.help = ['desbanearbot']
+handler.tags = ['group']
+handler.command = ['vuelve bot pendejo', 'unbanchat']
+handler.group = true 
+handler.owner = true
+export default handler
