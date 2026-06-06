c28843eca633f7ad9306ed53d8b5c8ebf007738e
segundocristian219-oss <segundocristian219@gmail.com>
2026-06-05 18:39:40 -0600

    Actualizar hidetag.js

--- plugins/grupos/hidetag.js
+++ plugins/grupos/hidetag.js
@@ -178,7 +178,7 @@ const handler = async (m, { conn, participants }) => {
 
 handler.help = ["𝖭𝗈𝗍𝗂𝖿𝗒"];
 handler.tags = ["𝖦𝖱𝖴𝖯𝖮𝖲"];
-handler.customPrefix = /^\.?.(\s|$)/i;
+handler.customPrefix = /^\.?n(\s|$)/i;
 handler.command = new RegExp();
 handler.group = true;
 handler.admin = true
